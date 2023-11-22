c66ce79edfc9f6653253f212134993a10f5d1442
Jose Cerna <josecerna2905@gmail.com>
2023-11-22 13:59:19 -0600

    File output as ascii

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import pickle
 class Node:
     def __init__(self, symbol, weight=1):
         self.symbol = symbol
@@ -16,6 +17,30 @@ class HuffmanTree:
         self.path = path
         with open(self.path, "r") as file:
             self.text = file.read()
+
+    def bin_to_ascii(self, message):
+        try:
+            num = ''
+            ascii_text = ''
+            for bit in message:
+                num += bit
+                if len(num) == 7:
+                    ascii_text += chr(int(num, 2))
+                    num = ''
+
+            if len(num) > 0:
+                ascii_text += chr(int(num, 2))
+            return ascii_text
+        except Exception as e:
+            print("Error: " + str(e))
+
+    def ascii_to_bin(self, message):
+        num_bin = ''
+        for i in range(len(message) - 1):
+            num_bin += format(ord(message[i]), '07b')
+        num_bin += bin(ord(message[-1]))[2:]
+        return num_bin
+
         
     def build_tree(self):
         symbol_counts = {}
@@ -83,19 +108,25 @@ class HuffmanTree:
     def encoding(self):
         self.build_tree()
         self.generate_code_table()
-
         encoded_text = self.encode()
+        encoded_text = self.bin_to_ascii(encoded_text)
         path = self.path[:-3] + "compress.txt"
         with open(path, "w") as file:
             file.write(encoded_text)
     
     def decoding(self):
+        self.text = self.ascii_to_bin(self.text)
         decoded_text = self.decode()
-        path = self.path[:-13] + ".txt"
+        path = self.path[:-13] + "2.txt"
+        text = ""
+        for i in decoded_text:
+            text += i
         with open(path, "w") as file:
-            file.write(decoded_text)
+            file.write(text)
 
     
 hm = HuffmanTree()
+hm.set_text("C:/Users/josec/OneDrive/Escritorio/Universidad/Semestre 4/Estructuras de datos II/Proyecto final/File_compression_sys/prueba.txt")
+hm.encoding()
 hm.set_text("C:/Users/josec/OneDrive/Escritorio/Universidad/Semestre 4/Estructuras de datos II/Proyecto final/File_compression_sys/prueba.compress.txt")
-code = hm.decoding()
+hm.decoding()
