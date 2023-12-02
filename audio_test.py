c228906caed0e4b9a1ef0f29da3af1908f9a8b20
AtzinCruz <124268669+AtzinCruz@users.noreply.github.com>
2023-12-01 19:16:29 -0600

    export bin from audio

--- audio_test.py
+++ audio_test.py
@@ -28,9 +28,14 @@ def binary_to_audio(binary_data, output_file):
     )
     audio.export(output_file, format="mp3", bitrate="192k")  # Ajustar la tasa de bits según tus necesidades
 
+def save_binary_data_to_file(binary_data, output_file):
+    with open(output_file, 'wb') as file:
+        file.write(binary_data)
+
 # Ejemplo de uso:
 audio_file = "test.mp3"
 output_audio_file = "output_audio.mp3"
+output_file = "output_audio.bin"
 
 # Convertir audio MP3 a binario
 binary_data = audio_to_binary(audio_file)
@@ -41,5 +46,6 @@ text_data = binary_to_text(binary_data)
 # Convertir texto a binario
 new_binary_data = text_to_binary(text_data)
 
+save_binary_data_to_file(binary_data, output_file)
 # Convertir binario a audio en formato MP3
 binary_to_audio(new_binary_data, output_audio_file)
